21b872d4d4eb5f47c49f7434da9f161d0dbbb1dc
EmmVega <emmanuelvegamecinas@gmail.com>
2025-06-18 23:12:48 -0600

    git init

---
+++ filemanager/filemanager.go
@@ -0,0 +1,67 @@
+package filemanager
+
+import (
+	"bufio"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+)
+
+type Filemanager struct {
+	InputFilePath  string
+	OutputFilePath string
+}
+
+func (fm Filemanager) ReadFiles() ([]string, error) {
+	file, err := os.Open(fm.InputFilePath)
+
+	if err != nil {
+		fmt.Println("Could not open file!")
+		fmt.Println(err)
+		return nil, errors.New("failed to open file")
+	}
+
+	scaner := bufio.NewScanner(file)
+
+	var lines []string
+
+	for scaner.Scan() {
+		lines = append(lines, scaner.Text())
+	}
+
+	err = scaner.Err()
+
+	if err != nil {
+		file.Close()
+		return nil, errors.New("failed to read file")
+	}
+
+	os.Stdin.Close()
+	return lines, nil
+}
+
+func (fm Filemanager) WriteJSON(data interface{}) error {
+	file, err := os.Create(fm.OutputFilePath)
+
+	if err != nil {
+		return errors.New("fail to create file")
+	}
+
+	encoder := json.NewEncoder(file)
+	err = encoder.Encode(data)
+
+	if err != nil {
+		file.Close()
+		return errors.New("fail to convert data to json")
+	}
+
+	return nil
+}
+
+func New(inputPath string, outputPath string) Filemanager {
+	return Filemanager{
+		InputFilePath:  inputPath,
+		OutputFilePath: outputPath,
+	}
+}
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"calculator-app/filemanager"
+	"calculator-app/prices"
+	"fmt"
+)
+
+func main() {
+	taxRate := []float64{0, 0.07, 0.1, 0.15}
+
+	for _, taxRate := range taxRate {
+		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
+		priceJob := prices.NewTaxIncludedPriceJob(fm, taxRate)
+		priceJob.Process()
+	}
+}
